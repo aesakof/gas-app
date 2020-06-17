0156f70548ee33e3ae9b8e018e2b930a2305b988
aesakof <aesakof@gmail.com>
2020-06-16 21:00:52 -0400

    Update views to query based on the username passed, update templates to conditionally show some elements depending if the page belongs to the logged-in user

--- fillups/urls.py
+++ fillups/urls.py
@@ -8,23 +8,12 @@ urlpatterns = [
     path('profile/<str:username>/fillups/',views.UserFillupListView.as_view(),name='user_fillup_list'),
     path('profile/<str:username>/cars/',views.UserCarListView.as_view(),name='user_car_list'),
     path('profile/<str:username>/stats/',views.UserStatsView.as_view(),name='user_stats'),
-    path('profile/<str:username>/fillups/edit/<int:pk>/',views.UpdateFillup.as_view(success_url='/profile/<str:username>/fillups/'),name='update_fillup'),
+    path('profile/<str:username>/fillups/edit/<int:pk>/',views.UpdateFillup.as_view(),name='update_fillup'),
     path('profile/<str:username>/fillups/delete/<int:pk>/',views.FillupDeleteView.as_view(),name='delete_fillup'),
-    path('profile/<str:username>/cars/edit/<int:pk>',views.UpdateCar.as_view(success_url='/profile/<str:username>/cars/'),name='update_car'),
+    path('profile/<str:username>/cars/edit/<int:pk>',views.UpdateCar.as_view(),name='update_car'),
     path('profile/<str:username>/cars/delete/<int:pk>',views.CarDeleteView.as_view(),name='delete_car'),
     path('all/fillups/',views.AllFillupListView.as_view(),name='all_fillup_list'),
     path('all/cars/',views.AllCarListView.as_view(),name='all_car_list'),
-    path('new/fillup',views.NewFillup.as_view(success_url='/profile/fillups/'),name='new_fillup'),
-    path('new/car',views.NewCar.as_view(success_url='/profile/cars/'),name='new_car')
+    path('new/fillup',views.NewFillup.as_view(),name='new_fillup'),
+    path('new/car',views.NewCar.as_view(),name='new_car')
 ]
-
-
-
-    # path('myprofile/',views.UserProfile.as_view(),name='user_profile'),
-    # path('myprofile/fillups/',views.UserFillupListView.as_view(),name='user_fillup_list'),
-    # path('myprofile/cars/',views.UserCarListView.as_view(),name='user_car_list'),
-    # path('myprofile/stats/',views.UserStatsView.as_view(),name='user_stats'),
-    # path('myprofile/fillups/edit/<int:pk>/',views.UpdateFillup.as_view(success_url='/myprofile/fillups/'),name='update_fillup'),
-    # path('myprofile/fillups/delete/<int:pk>/',views.FillupDeleteView.as_view(),name='delete_fillup'),
-    # path('myprofile/cars/edit/<int:pk>',views.UpdateCar.as_view(success_url='/myprofile/cars/'),name='update_car'),
-    # path('myprofile/cars/delete/<int:pk>',views.CarDeleteView.as_view(),name='delete_car'),
--- fillups/views.py
+++ fillups/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (TemplateView,ListView,DetailView,
                                     CreateView,UpdateView,DeleteView)
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.db.models import Sum, Avg, F, Func
 from accounts.models import User
 from fillups.models import Fillup, Car
@@ -33,13 +33,16 @@ class UserProfile(TemplateView):
             'total_spent': sum_total_sale(Fillup.objects.filter(username=usr)),
             'avg_mpg': avg_mpg(Fillup.objects.filter(username=usr))
         }
+        context['profile_name'] = usr.username
         context['stats'] = overview_stats
         context['active_cars'] = Car.objects.filter(status='Active').filter(username=usr)
         context['last_10_fillups'] = Fillup.objects.filter(username=usr).order_by('-date')[:10]
         return context
 
+
 class UserStatsView(TemplateView):
     template_name = 'fillups/user_stats.html'
+
 
 class UserFillupListView(ListView):
     template_name = 'fillups/user_fillup_list.html'
@@ -48,7 +51,15 @@ class UserFillupListView(ListView):
     ordering = ['-date']
 
     def get_queryset(self):
-        return Fillup.objects.filter(username=self.request.user).order_by('-date')
+        usr = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Fillup.objects.filter(username=usr).order_by('-date')
+
+    def get_context_data(self):
+        usr = get_object_or_404(User, username=self.kwargs.get("username"))
+        context = super(UserFillupListView,self).get_context_data(**self.kwargs)
+        context['profile_name'] = usr.username
+        return context
+
 
 class AllFillupListView(ListView):
     template_name = 'fillups/all_fillup_list.html'
@@ -56,10 +67,10 @@ class AllFillupListView(ListView):
     context_object_name = 'all_fillup_list'
     ordering = ['-date']
 
+
 class NewFillup(LoginRequiredMixin,CreateView):
     model = Fillup
     form_class = forms.FillupForm
-    redirect_field_name = 'fillups:fillup_list'
 
     def form_valid(self, form):
         form.instance.username = self.request.user
@@ -69,11 +80,14 @@ class NewFillup(LoginRequiredMixin,CreateView):
         kwargs = super().get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
+
+    def get_success_url(self):
+        return reverse('fillups:user_fillup_list', kwargs={'username': self.object.username})
+
 
 class UpdateFillup(LoginRequiredMixin,TestUserOfObject,UpdateView):
     model = Fillup
     form_class = forms.FillupForm
-    redirect_field_name = 'fillups:fillup_list'
 
     def form_valid(self, form):
         form.instance.username = self.request.user
@@ -84,9 +98,16 @@ class UpdateFillup(LoginRequiredMixin,TestUserOfObject,UpdateView):
         kwargs['user'] = self.request.user
         return kwargs
 
+    def get_success_url(self):
+        return reverse('fillups:user_fillup_list', kwargs={'username': self.object.username})
+
+
 class FillupDeleteView(LoginRequiredMixin,TestUserOfObject,DeleteView):
     model = Fillup
-    success_url = reverse_lazy('fillups:user_fillup_list')
+
+    def get_success_url(self):
+        return reverse('fillups:user_fillup_list', kwargs={'username': self.object.username})
+
 
 class UserCarListView(ListView):
     template_name = 'fillups/user_car_list.html'
@@ -95,7 +116,15 @@ class UserCarListView(ListView):
     ordering = ['name']
 
     def get_queryset(self):
-        return Car.objects.filter(username=self.request.user).order_by('name')
+        usr = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Car.objects.filter(username=usr).order_by('name')
+
+    def get_context_data(self):
+        usr = get_object_or_404(User, username=self.kwargs.get("username"))
+        context = super(UserCarListView,self).get_context_data(**self.kwargs)
+        context['profile_name'] = usr.username
+        return context
+
 
 class AllCarListView(ListView):
     template_name = 'fillups/all_car_list.html'
@@ -103,14 +132,18 @@ class AllCarListView(ListView):
     context_object_name = 'all_car_list'
     ordering = ['name']
 
+
 class NewCar(LoginRequiredMixin,CreateView):
     model = Car
     form_class = forms.CarForm
-    redirect_field_name = 'fillups:car_list'
 
     def form_valid(self, form):
         form.instance.username = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('fillups:user_car_list', kwargs={'username': self.object.username})
+
 
 class UpdateCar(LoginRequiredMixin,TestUserOfObject,UpdateView):
     model = Car
@@ -121,9 +154,15 @@ class UpdateCar(LoginRequiredMixin,TestUserOfObject,UpdateView):
         form.instance.username = self.request.user
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('fillups:user_car_list', kwargs={'username': self.object.username})
+
+
 class CarDeleteView(LoginRequiredMixin,TestUserOfObject,DeleteView):
     model = Car
-    success_url = reverse_lazy('fillups:user_car_list')
+
+    def get_success_url(self):
+        return reverse('fillups:user_car_list', kwargs={'username': self.object.username})
 
 ###############################################################################
 
